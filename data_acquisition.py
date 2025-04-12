8ac9150ce0287dcea7bda4a1d1652b181e0396a0
Riya Aktar <aktarr4@uni.coventry.ac.uk>
2025-04-12 15:21:30 +0100

    Update data_acquisition.py

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,1 +1,9 @@
+#using pandas for data acquisition
+import pandas as pd
 
+#Load dataset using Pandas
+df = pd.read_csv("Trips_by_Distance.csv")
+
+#Inspect dataset
+print(df.head())
+print(df.info())
