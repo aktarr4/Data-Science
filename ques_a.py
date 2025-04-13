78ae46d156594f9490f224409455c8ec413f8dac
Riya Aktar <aktarr4@uni.coventry.ac.uk>
2025-04-13 13:20:05 +0100

    Update ques_a.py

--- ques_a.py
+++ ques_a.py
@@ -1,9 +1,32 @@
-#using pandas for data acquisition
+# Import required libraries
 import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
 
-#Load dataset using Pandas
-df = pd.read_csv("Trips_by_Distance.csv")
+df_trips = pd.read_csv('Trips_by_Distance.csv')   
 
-#Inspect dataset
-print(df.head())
-print(df.info())
+#average Population Staying at Home 
+print(df_trips[df_trips['Level'] == 'National']['Population Staying at Home'].mean().round())
+
+# Use average distance for each trip range
+df_trips['Total Distance'] = (
+    df_trips['Number of Trips <1'] * 0.5 +
+    df_trips['Number of Trips 1-3'] * 2 +
+    df_trips['Number of Trips 3-5'] * 4 +
+    df_trips['Number of Trips 5-10'] * 7.5 +
+    df_trips['Number of Trips 10-25'] * 17.5 +
+    df_trips['Number of Trips 25-50'] * 37.5 +
+    df_trips['Number of Trips 50-100'] * 75 +
+    df_trips['Number of Trips 100-250'] * 175 +
+    df_trips['Number of Trips 250-500'] * 375 +
+    df_trips['Number of Trips >=500'] * 600
+)
+
+# Calculating average distance per person who weren't home
+df_trips['Avg Distance Per Person'] = df_trips['Total Distance'].sum() / df_trips['Population Not Staying at Home'].sum()
+
+print(df_trips["Avg Distance Per Person"].mean().round())
